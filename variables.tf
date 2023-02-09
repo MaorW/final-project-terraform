2a19fa09487e03c7e42f044cdcc415ec319aa8e5
maor weiss <maorweissb@gmail.com>
2023-02-09 10:49:28 +0200

    Guiding the user on what variable values should be set

--- variables.tf
+++ variables.tf
@@ -2,11 +2,11 @@ variable "awsprops" {
     type = map(string)
     default = {
       profile = "default"
-      region = "us-east-1"
-      vpc = "vpc-0bbbab9eac57c27fc"
+      region = "us-east-1" # You should change to your region ID
+      vpc = "vpc-0bbbab9eac57c27fc" # You should change to your VPC ID
       ami = "ami-0aa7d40eeae50c9a9"
       itype = "t2.micro"
-      subnet = "subnet-01e0b38bdcc37bbd4"
+      subnet = "subnet-01e0b38bdcc37bbd4" # You should change to your subnet ID
       publicip = true
       keyname = "final-project-key"
       secgroupname = "react-Sec-Group"
